940e41201f63bef05768632d11686418be83927f
AlexReisC <alex.reis@aluno.ufca.edu.br>
2023-06-22 16:04:34 -0300

    :sparkle:feat: terminando Lista

--- src/estruturasDeDados/Lista.java
+++ src/estruturasDeDados/Lista.java
@@ -4,7 +4,7 @@ public class Lista {
     private No primeiroNo;
     private No ultimoNo;
 
-    public Lista(No primeiroNo, No ultimoNo) {
+    public Lista() {
         this.primeiroNo = null;
         this.ultimoNo = null;
     }
@@ -32,7 +32,6 @@ public class Lista {
     public void inserirInicio(int valor){
         if(isEmpity()){
             primeiroNo = ultimoNo = new No(null, valor);
-            ;
         } else{
             primeiroNo = new No(primeiroNo, valor);
         }
@@ -42,15 +41,61 @@ public class Lista {
         if(isEmpity()){
             primeiroNo = ultimoNo = new No(null, valor);
         } else{
-            ultimoNo.setProximoNo(new No(ultimoNo, valor));
+            ultimoNo.setProximoNo(new No(null, valor));
             ultimoNo = ultimoNo.getProximoNo();
         }
     }
-    
-    public void remover(){
 
+    public boolean buscar(int valor){
+        No aux = primeiroNo;
+
+        while(aux != null){
+            if (aux.getValor() == valor) {
+                System.out.println("Encontrado!");
+                return true;
+            }
+            aux = aux.getProximoNo();
+        }
+        System.out.println("Não encontrado!");
+        return false;
+    }
+    
+    public boolean remover(int valor){
+        No atual = primeiroNo;
+        while (atual != null) {
+            if (atual.getValor() == valor) {
+                No anterior = primeiroNo;
+                while (anterior != null) {
+                    if (anterior.getProximoNo() == atual) {
+                        anterior.setProximoNo(atual.getProximoNo());
+                        return true;
+                    } else if(anterior == atual){
+                        primeiroNo = atual.getProximoNo();
+                        return true;
+                    }
+                    anterior = anterior.getProximoNo();
+                }
+            }
+            atual = atual.getProximoNo();
+        }
+        return false;
     }
 
-    
+    public void imprimir(){
+        No aux = primeiroNo;
+        while (aux != null) {
+            System.out.print(aux.getValor() + " ");
+            aux = aux.getProximoNo();
+        }
+    }
+
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        lista.inserirInicio(1);
+        lista.inserirFim(2);
+        lista.inserirInicio(5);
+        lista.remover(2);
+        lista.imprimir();
+    }
 
 }
